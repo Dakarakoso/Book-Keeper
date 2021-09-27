b9cdcd599657f53868e1351c1a927becf1479201
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-09-27 13:40:41 +0900

    implemented form validation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function showModal() {
   websiteNameEl.focus();
 }
 
-// add event listener
+// modal event listener
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", () =>
   modal.classList.remove("show-modal")
@@ -20,3 +20,37 @@ modalClose.addEventListener("click", () =>
 window.addEventListener("click", (e) =>
   e.target === modal ? modal.classList.remove("show-modal") : false
 );
+
+// validate url format using regex
+function validateUrl(nameUrl, urlValue) {
+  const expression =
+    /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameUrl || !urlValue) {
+    alert("Error: uncompleted fields");
+  }
+  if (urlValue.match(regex)) {
+    alert("match");
+  }
+  if (!urlValue.match(regex)) {
+    alert("Error: not a valid website url");
+    return false;
+  }
+  return true;
+}
+
+const storeBookmark = (e) => {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes("https://") && !urlValue.includes("http://")) {
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue);
+  if (!validateUrl(nameValue, urlValue)) {
+    return false;
+  }
+};
+
+// event listener
+bookmarkForm.addEventListener("submit", storeBookmark);
